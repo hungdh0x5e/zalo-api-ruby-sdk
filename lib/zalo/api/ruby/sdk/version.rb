7dead38d9a3f1fb59c3637f2011e17556ac2b738
Huy Hùng <hungdh@onaclover.com>
2019-11-13 16:13:23 +0700

    init gem

---
+++ lib/zalo/api/ruby/sdk/version.rb
@@ -0,0 +1,9 @@
+module Zalo
+  module Api
+    module Ruby
+      module Sdk
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
---
+++ spec/zalo/api/ruby/sdk_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Zalo::Api::Ruby::Sdk do
+  it "has a version number" do
+    expect(Zalo::Api::Ruby::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
