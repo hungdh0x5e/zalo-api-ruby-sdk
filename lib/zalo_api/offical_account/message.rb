a0c06ebf08da25e819059a6fbed54b76f4dabb16
Huy Hùng <hungdh@onaclover.com>
2019-11-18 15:05:52 +0700

    oa: send text message to user_id

--- lib/zalo_api/offical_account/message.rb
+++ lib/zalo_api/offical_account/message.rb
@@ -2,12 +2,20 @@ module ZaloAPI::OfficalAccount
   module Message
     # Send message to user_id
     # @param [String] user_id
+    # @param [String] message
     def send_text_message(user_id, message)
-      recipient = { user_id: user_id }
-      message = { text: message }
+      execute(user_id, message)
+    end
+
+    private
+
+    def execute(user_id, text, attachment = {})
       payload = {
-        recipient: recipient,
-        message: message
+        recipient: { user_id: user_id },
+        message: {
+          text: text,
+          attachment: attachment
+        }
       }
 
       @client.connection.post "#{base_url}/oa/message", payload
